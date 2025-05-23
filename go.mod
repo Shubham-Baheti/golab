ac37e15bf71ee9299d4988fb7e669148f70c636d
Shubham-Baheti8 <shubham.baheti@brevo.com>
2025-05-23 22:20:54 +0530

    first code of go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Shubham-Baheti/golab
+
+go 1.24.1
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func defination() string{
+	return "Hello,World"
+}
+
+func main(){
+	var temp string = defination() 
+	fmt.Println(temp)
+}
+
