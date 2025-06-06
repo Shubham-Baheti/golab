8dec40056709f9199cd6e7c7123a00b62d531e63
Shubham-Baheti8 <shubham.baheti@brevo.com>
2025-06-06 11:24:00 +0530

    learn function and range function

--- main.go
+++ main.go
@@ -1,26 +1,21 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-)
-
-func swap(a, b string) (string, string) {
-	return b, a
-}
-
-func valueAssign(sum int) (int, int) {
-	var a int = sum * 4 / 9
-	var b int = sum*5 + 4
-	return a, b
-}
+import "fmt"
 
 func main() {
-	fmt.Println("Hello from goLearning")
-	fmt.Println("This is a random number:", rand.Intn(100))
-	a, b := swap("hello", "world")
-	println("Swapped values:", a, b)
-	x, y := valueAssign(17)
-	fmt.Println("Assigned values:", x, y)
-
+	names := []string{"Shubham", "Amit", "Ravi"}
+	for _, values := range names {
+		fmt.Println("Values ", values)
+	}
+	for ind := range names {
+		fmt.Println("Indexs ", ind)
+	}
+	for i := range names {
+		if i==0 {
+			names[i] = "Shubham Baheti"
+		} 
+	}
+	for _, values := range names {
+		fmt.Println("Values ", values)
+	}
 }
