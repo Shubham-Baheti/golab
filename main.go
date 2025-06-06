85b998376fa0b8253e9a87558973baebb707d22b
Shubham-Baheti8 <shubham.baheti@brevo.com>
2025-06-06 12:26:27 +0530

    if else switch or defer is coverd

--- main.go
+++ main.go
@@ -1,21 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	names := []string{"Shubham", "Amit", "Ravi"}
-	for _, values := range names {
-		fmt.Println("Values ", values)
+	score := 85
+	if score > 90 {
+		fmt.Println("Grade A")
+	} else if score > 75 {
+		fmt.Println("Grade B")
+	} else {
+		fmt.Println("Grade C or Below")
 	}
-	for ind := range names {
-		fmt.Println("Indexs ", ind)
+
+	if age := 20; age >= 18 {
+		fmt.Println("You can consume alcohol")
+	} else {
+		fmt.Println("You can not consume alcohol")
 	}
-	for i := range names {
-		if i==0 {
-			names[i] = "Shubham Baheti"
-		} 
+
+	if score := 80; score > 90 {
+		fmt.Println("Grade A+")
+	} else {
+		fmt.Println("Grade B-")
 	}
-	for _, values := range names {
-		fmt.Println("Values ", values)
+
+	lang := "go"
+
+	switch lang {
+	case "python":
+		fmt.Println("Python dev")
+	case "go":
+		fmt.Println("go dev")
+	default:
+		fmt.Println("Unknown dev")
 	}
+
+	score = 90
+
+	switch {
+	case score >= 90:
+		fmt.Println("Grade A")
+	case score >= 75:
+		fmt.Println("Grade B")
+	default:
+		fmt.Println("Grade C")
+	}
+
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+
+	fmt.Println("defer is done like a stack memory")
+
 }
