6fed5ed06c714a39a5408ef1db64ecb7a0934ce2
Shubham-Baheti8 <shubham.baheti@brevo.com>
2025-06-09 15:15:51 +0530

    Rectange area calculated using method

--- main.go
+++ main.go
@@ -2,13 +2,31 @@ package main
 
 import "fmt"
 
-func main() {
-	var a [2]string
-	a[0] = "shubham"
-	a[1] = "bhanu"
-	fmt.Println(a[0], a[1])
-	fmt.Println(a)
+// Make a Rectangle struct with Width and Height. Add a method:
 
-	primes := [6]int{2, 3, 4, 56, 7, 8}
-	fmt.Println(primes)
+// Area() float64 → returns area
+
+// Scale(f float64) → scales both width and height
+
+type Rectangle struct {
+	Height, Width float64
+}
+
+func (rec *Rectangle) Area() float64 {
+	area := rec.Height * rec.Width
+	return area
+}
+
+func (rec *Rectangle) Scale(f float64) {
+	rec.Height = rec.Height * f
+	rec.Width = rec.Width * f
+}
+
+func main() {
+	rec := Rectangle{3, 4}
+	fmt.Printf("the initial height=%.2f and width=%.2f\n", rec.Height, rec.Width)
+	rec.Scale(5)
+	fmt.Printf("after scale height=%.2f and width=%.2f\n", rec.Height, rec.Width)
+	fmt.Println("The area is ", rec.Area())
+
 }
