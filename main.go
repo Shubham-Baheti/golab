d8ea7b8827953fdbe6980d20adf804c1b7534432
Shubham-Baheti8 <shubham.baheti@brevo.com>
2025-06-10 18:54:28 +0530

    Generic example

--- main.go
+++ main.go
@@ -2,31 +2,36 @@ package main
 
 import "fmt"
 
-// Make a Rectangle struct with Width and Height. Add a method:
-
-// Area() float64 → returns area
-
-// Scale(f float64) → scales both width and height
-
-type Rectangle struct {
-	Height, Width float64
+func indexOfNum(s []int, x int) int {
+	for index, val := range s {
+		if val == x {
+			return index
+		}
+	}
+	return -1
 }
 
-func (rec *Rectangle) Area() float64 {
-	area := rec.Height * rec.Width
-	return area
+func indexOfStr(s []string, temp string) int {
+	for index, val := range s {
+		if val == temp {
+			return index
+		}
+	}
+	return -1
 }
 
-func (rec *Rectangle) Scale(f float64) {
-	rec.Height = rec.Height * f
-	rec.Width = rec.Width * f
+func indexOfAll[T comparable](slice []T, value T) int{
+	for index,val :=range slice{
+		if val == value {
+			return index
+		}
+	}
+	return -1;
 }
 
 func main() {
-	rec := Rectangle{3, 4}
-	fmt.Printf("the initial height=%.2f and width=%.2f\n", rec.Height, rec.Width)
-	rec.Scale(5)
-	fmt.Printf("after scale height=%.2f and width=%.2f\n", rec.Height, rec.Width)
-	fmt.Println("The area is ", rec.Area())
-
+	s := []int{3, 4, 5, 6, 7}
+	fmt.Println(indexOfAll(s, 7))
+	temp := []string{"Hello", "World", "Mohan", "Ram"}
+	fmt.Println(indexOfAll(temp, "Mohan"))
 }
